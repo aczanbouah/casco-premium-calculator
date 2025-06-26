e03be6422b80d3d7f6da08bc08d58f3dbd79bcdf
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2025-06-26 19:45:23 +0300

    Store vehicle form data into array on submit

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+const vehicleData = [];
+const vehicleDataForm = document.querySelector(".form");
+
 function InsuranceQuote(vehicleData, driverData, coverageOptions) {
   this.vehicleData = vehicleData;
   this.driverData = driverData;
   this.coverageOptions = coverageOptions;
 }
+
+vehicleDataForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(vehicleDataForm);
+  const obj = Object.fromEntries(formData);
+  vehicleData.push(obj);
+  vehicleDataForm.reset();
+});
